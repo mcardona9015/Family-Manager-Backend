43fd5a8ca1147b35c1108903c63de26e1c17b81a
mcardona9015 <mcardona9015@gmail.com>
2021-03-18 17:38:34 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+michael = User.create(username: "michael", password: "123")
+album = PhotoAlbum.create(user_id: michael.id, title: "sample album")
